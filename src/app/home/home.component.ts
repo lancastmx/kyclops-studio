428bc92e41eeef0135110835ae4d0eb986989649
Edy <94276786+EdySnakee@users.noreply.github.com>
2025-04-28 20:05:23 -0600

    [add
    q
    
    |

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "../components/hero/hero.component";
 import { CardsServiciosComponent } from "../components/cards-servicios/cards-servicios.component";
 import { PreciosComponent } from "../components/precios/precios.component";
+import { FooterComponent } from "../components/footer/footer.component";
+import { AboutComponent } from "../components/about/about.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, CardsServiciosComponent, PreciosComponent],
+  imports: [HeroComponent, CardsServiciosComponent, PreciosComponent, FooterComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
