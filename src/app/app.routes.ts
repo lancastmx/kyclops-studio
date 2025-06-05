539d7e034af52fe5d51cc538026248795a1afab7
lancast <angelprep92@gmail.com>
2025-06-04 19:31:39 -0600

    feat:landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: LandingpageComponent,
   }
 ];
